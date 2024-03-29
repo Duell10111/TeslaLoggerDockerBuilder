bd233787d57d5c1712f6b5e29f972b6eb19e5d25
Konstantin Späth <duell10111@t-online.de>
2024-03-29 19:50:07 +0100

    chore: adapt image check

--- imageBuild.sh
+++ imageBuild.sh
@@ -2,20 +2,24 @@
 
 ## Testing
 #REGISTRY=ghcr.io
-#WEB_SERVER_IMAGE_NAME="webserver"
-#TESLA_LOGGER_IMAGE_NAME="tesla-logger"
 ## ---
 
+WEB_SERVER_IMAGE_NAME="webserver"
+TESLA_LOGGER_IMAGE_NAME="tesla-logger"
+
 IMAGE_PREFIX="duell10111/teslalogger"
-LOCAL_WEBSERVER_IMAGE="teslalogger-webserver:latest"
-LOCAL_TESLALOGGER_IMAGE="teslalogger-teslalogger:latest"
+LOCAL_WEBSERVER_IMAGE="teslalogger_webserver:latest"
+LOCAL_TESLALOGGER_IMAGE="teslalogger_teslalogger:latest"
 
 git clone https://github.com/bassmaster187/TeslaLogger
 cd TeslaLogger || exit 1
 LATEST_TAG=$(git tag | sort --version-sort | tail -n1)
 
-if ! (docker manifest inspect "$REGISTRY/$IMAGE_PREFIX/$WEB_SERVER_IMAGE_NAME:$LATEST_TAG" || docker manifest inspect "$REGISTRY/$IMAGE_PREFIX/$TESLA_LOGGER_IMAGE_NAME:$LATEST_TAG"); then
+if ! docker manifest inspect "$REGISTRY/$IMAGE_PREFIX/$WEB_SERVER_IMAGE_NAME:$LATEST_TAG" || ! docker manifest inspect "$REGISTRY/$IMAGE_PREFIX/$TESLA_LOGGER_IMAGE_NAME:$LATEST_TAG"; then
   echo "Images not found for tag $LATEST_TAG"
+else
+  echo "Beide Befehle waren erfolgreich (Exit-Code 0)."
+  echo "Images already existing"
   exit 1
 fi
 
